5fb2192c9856ac55b207d938aec18033340a0a5e
bansalanjali2512 <31640108+bansalanjali2512@users.noreply.github.com>
2017-10-10 23:56:50 +0530

    Create sizeofDataTypes.cpp

---
+++ sizeofDataTypes.cpp
@@ -0,0 +1,27 @@
+//Program to print size of data types
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  cout << "Size of Data Types : " << "\n\n";
+  cout << "sizeof(char) : " << sizeof(char) << "\n";
+	cout << "sizeof(signed char) : " << sizeof(signed char) << "\n";
+	cout << "sizeof(unsigned char) : " << sizeof(unsigned char) << "\n";
+	cout << "sizeof(int) : " << sizeof(int) << "\n";
+	cout << "sizeof(signed int) : " << sizeof(signed int) << "\n";
+	cout << "sizeof(unsigned int) : " << sizeof(unsigned int) << "\n";
+	cout << "sizeof(short int) : " << sizeof(short int) << "\n";
+	cout << "sizeof(signed short int) : " << sizeof(signed short int) << "\n";
+	cout << "sizeof(unsigned short int) : " << sizeof(unsigned short int) << "\n";
+	cout << "sizeof(long int) : " << sizeof(long int) << "\n";
+	cout << "sizeof(signed long int) : " << sizeof(signed long int) << "\n";
+	cout << "sizeof(unsigned long int) : " << sizeof(unsigned long int) << "\n";
+	cout << "sizeof(float) : " << sizeof(float) << "\n";
+	cout << "sizeof(double) : " << sizeof(double) << "\n";
+	cout << "sizeof(long double) : " << sizeof(long double) << "\n";
+	cout << "sizeof(wchar_t) : " << sizeof(wchar_t) << "\n";
+	
+  return 0;
+}
