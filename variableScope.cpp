20d29a8c3b3eb0155d082a0bf778944d6881d705
bansalanjali2512 <31640108+bansalanjali2512@users.noreply.github.com>
2017-10-18 23:56:32 +0530

    Create variableScope.cpp

---
+++ variableScope.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int globalVar; //global variable
+
+int main () {
+   int localVar1, localVar2; //local variables
+ 
+   localVar1=20;
+   localVar2=30;
+   globalVar=localVar1+localVar2;
+  
+   cout << "Values Are : \n ";
+   cout << "globalVar : " << globalVar << "\n";
+   cout << "localVar1 : " << localVar1 << "\n";
+   cout << "localVar2 : " << localVar2 << "\n";
+   
+   return 0;
+} 
