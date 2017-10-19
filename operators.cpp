43e59f4e795ffa39de02a6473a7defcb1e3d28e9
bansalanjali2512 <31640108+bansalanjali2512@users.noreply.github.com>
2017-10-19 23:59:29 +0530

    Update operators.cpp

--- operators.cpp
+++ operators.cpp
@@ -4,12 +4,8 @@ using namespace std;
 int main(){
 
 	int testValue = 15;
-	cout << testValue << endl;
-	testValue >>= 2; 			//Right Shift by 2
-	cout << testValue << endl;
-	testValue <<= 2; 			//Left Shift by 4
-	cout << testValue << endl;
-
+	
+	
 	testValue > 0 ? cout << "Yes" : cout << "No"; 			// (condition ? if true : else)  -  One liner if else.
 
 	float floatValue = 3.1415;
