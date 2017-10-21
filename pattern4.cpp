9d36338cdd5ffb576564d6b7eafebefa5b5518f8
bansalanjali2512 <anjali.mca17.du@gmail.com>
2017-10-21 23:27:44 +0530

    Program to print pattern

---
+++ pattern4.cpp
@@ -0,0 +1,35 @@
+/*
+  This program prints following pattern:
+  *****
+   ***
+    *
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int nRows;
+
+  label:
+  cout << "Enter number of rows you want to print: ";
+  cin >> nRows;
+  if (nRows < 0) {
+    cout << "Oops..Enter a positive number..!\n";
+    goto label;
+  }
+
+  int nSpace=0;
+  for (int i=nRows; i>=1; i--) {
+    for (int k=nSpace; k>=1; k--) {
+      cout << " ";
+    }
+    for (int j=(2*i-1); j>=1; j--) {
+      cout << "*";
+    }
+    cout << "\n";
+    nSpace++;
+  }
+
+  return 0;
+}
