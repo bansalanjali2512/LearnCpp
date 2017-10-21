ce07c9c7d6deee4a1b2597691110f060132128c5
DeltaSquareDUCS <anjali.mca17.du@gmail.com>
2017-10-21 18:19:07 +0530

    Uploading pattern1.cpp

---
+++ pattern1.cpp
@@ -0,0 +1,30 @@
+/*
+  This program prints following pattern:
+  *
+  **
+  ***
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int nRows;
+
+  label:
+  cout << "Enter number of rows you want to print: ";
+  cin >> nRows;
+  if (nRows < 0) {
+    cout << "Oops..Enter a positive number..!\n";
+    goto label;
+  }
+
+  for (int i=1; i<=nRows; i++) {
+    for (int j=1; j<=i; j++) {
+        cout << "*";
+    }
+    cout << "\n";
+  }
+
+  return 0;
+}
