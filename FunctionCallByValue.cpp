05fcb70a2239c95587d9e097b3faa6194de5278b
bansalanjali2512 <anjali.mca17.du@gmail.com>
2017-10-29 23:51:17 +0530

    Swap numbers using call by value.

---
+++ FunctionCallByValue.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+//function declaration
+void swapNumbers(int num1, int num2);
+
+int main () {
+   int num1, num2;
+
+   cout << " Enter number 1: ";
+   cin >> num1;
+   cout << " Enter number 2: ";
+   cin >> num2;
+
+   //calling function
+   swapNumbers(num1, num2);
+   cout << "\n\n Inside Main: \n";
+   cout << "\n num1: " << num1;
+   cout << "\n num2: " << num2;
+
+   return 0;
+}
+
+//function defination
+void swapNumbers(int num1, int num2) {
+   int temp;
+   temp = num1;
+   num1 = num2;
+   num2 = temp;
+   cout << "\n Inside swapNumbers Function: \n";
+   cout << "\n num1: " << num1;
+   cout << "\n num2: " << num2;
+}
