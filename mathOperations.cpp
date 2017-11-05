91b9f47345c989a380e2dfb59557c9b0980072ae
bansalanjali2512 <anjali.mca17.du@gmail.com>
2017-11-06 00:09:15 +0530

    Adding more operations.

--- mathOperations.cpp
+++ mathOperations.cpp
@@ -1,3 +1,5 @@
+//Program to perform math operations
+
 #include <iostream>
 #include <cmath>
 using namespace std;
@@ -17,6 +19,23 @@ int main() {
   //This function takes a number and returns the natural log of that number.
   cout << " log(200.374): " << log(200.374) << "\n";
 
+  //The first is a number you wish to raise and the second is the power you wish to raise it t
+  cout << " pow(2,3): " << pow(2,3) << "\n";
+
+  //If you pass this function the length of two sides of a right triangle, it will return you the length of the hypotenuse.
+  cout << " hypot(5,12): " << hypot(5,12) << "\n";
+
+  //You pass this function a number and it gives you the square root.
+  cout << " sqrt(200.374): " << sqrt(200.374) << "\n";
+
+  //This function returns the absolute value of an integer that is passed to it.
+  cout << " abs(-200): " << abs(-200) << "\n";
+
+  //This function returns the absolute value of any decimal number passed to it.
+  cout << " fabs(200.374): " << fabs(200.374) << "\n";
+
+  //Finds the integer which is less than or equal to the argument passed to it.
+  cout << " floor(200.374): " << floor(200.374) << "\n";
 
   return 0;
 }
