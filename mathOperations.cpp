ff83eee2e3931a87b9ec737a1ce35709fb4a2c55
bansalanjali2512 <anjali.mca17.du@gmail.com>
2017-11-05 23:55:28 +0530

    Math Operations.

---
+++ mathOperations.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+
+  cout << " *** Math Operations *** \n\n";
+  //This function takes an angle (as a double) and returns the cosine.
+  cout << " cos(200.374): " << cos(200.374) << "\n";
+
+  //This function takes an angle (as a double) and returns the sine.
+  cout << " sin(200.374): " << sin(200.374) << "\n";
+
+  //This function takes an angle (as a double) and returns the tangent.
+  cout << " tan(200.374): " << tan(200.374) << "\n";
+
+  //This function takes a number and returns the natural log of that number.
+  cout << " log(200.374): " << log(200.374) << "\n";
+
+
+  return 0;
+}
