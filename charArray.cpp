74f3bc5aab39361863ba88e39c6b5f4dbfd9b33a
bansalanjali2512 <anjali.mca17.du@gmail.com>
2017-11-08 23:57:56 +0530

    Character array.

---
+++ charArray.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+   char charArray[12] = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '\0'};
+   cout << " Welcome Message : " << charArray << endl;
+
+   return 0;
+}
