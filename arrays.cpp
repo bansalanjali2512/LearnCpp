a0c5dcf644ef96d374e13d542bc2db15b284542e
Anjali Bansal <anjali.mca17.du@gmail.com>
2017-11-10 23:46:50 +0530

    Update arrays.cpp
    Inputs and display array.

--- arrays.cpp
+++ arrays.cpp
@@ -1,1 +1,20 @@
-//Array
+//This program inputs array from user and display it
+#include <iostream>
+using namespace std;
+
+int main () {
+   int arr[10]; //Array declaration of size 10
+
+   //Input array elements
+   cout << "Enter Array Elements: \n";
+   for(int i = 0; i < 10; i++) {
+      cin >> arr[i];
+   }
+
+   //Display array elements
+   cout << "\nArray Is: ";
+   for(int i = 0; i < 10; i++ ) {
+      cout << arr[i] << " ";
+   }
+   return 0;
+}
